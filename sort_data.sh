9467f3f979be95dc6e16981fd45701fba2cc5a58
Antonio Feregrino <antonio.feregrino@gmail.com>
2023-03-11 22:45:56 +0000

    Attempt at data sorting #3

--- sort_data.sh
+++ sort_data.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
+# Change directory to the 'data' folder
 cd data
 
-for file in *.csv
-do
-  # create temporary file for sorting
-  tmpfile=$(mktemp)
+# Loop through each CSV file in the folder
+for filename in *.csv; do
 
-  # extract the header row to a separate file
-  head -n 1 "$file" > header.csv
+  # Save the header row to a temporary file
+  head -n1 "$filename" > header.tmp
 
-  # sort the remaining rows numerically
-  tail -n +2 "$file" | sort -n > "$tmpfile"
+  # Sort the remaining lines in the file based on the first field, ignoring the header
+  tail -n+2 "$filename" | sort -t',' -k1 -o "$filename.tmp"
 
-  # concatenate the header row and sorted rows into a new file
-  cat header.csv "$tmpfile" > sorted.csv
+  # Re-attach the header row to the sorted data
+  cat header.tmp "$filename.tmp" > "$filename"
 
-  # overwrite the original file with the sorted output
-  mv sorted.csv "$file"
+  # Remove the temporary files
+  rm header.tmp "$filename.tmp"
 
-  # delete temporary files
-  rm header.csv "$tmpfile"
 done
